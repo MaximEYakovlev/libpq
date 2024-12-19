54222b9ab51f36aa28c632e4166b0f3ffedda3b7
Maxim Yakovlev <it.trainee@yandex.ru>
2024-12-19 23:54:08 +0300

    establishing a database connection

--- main.c
+++ main.c
@@ -3,5 +3,19 @@
 
 int main()
 {
-    return 0
+    const char *conninfo = "host=localhost dbname=mydb user=myuser password=mypassword";
+    PGconn *conn = PQconnectdb(conninfo);
+
+    if (PQstatus(conn) != CONNECTION_OK)
+    {
+        fprintf(stderr, "connection failed: %s\n", PQerrorMessage(conn));
+        PQfinish(conn);
+
+        return 1;
+    }
+
+    printf("connected to the database successfully.\n");
+    PQfinish(conn);
+
+    return 0;
 }
