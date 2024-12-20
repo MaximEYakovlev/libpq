160ebf37dbf57373b7eb1351517e471ef7fa8974
Maxim Yakovlev <it.trainee@yandex.ru>
2024-12-20 23:35:33 +0300

    query execution

--- main.c
+++ main.c
@@ -15,6 +15,20 @@ int main()
     }
 
     printf("connected to the database successfully.\n");
+
+    PGresult *res = PQexec(conn, "SELECT version();");
+
+    if (PQresultStatus(res) != PGRES_TUPLES_OK)
+    {
+        fprintf(stderr, "SELECT query failed: %s\n", PQerrorMessage(conn));
+        PQclear(res);
+        PQfinish(conn);
+
+        return 1;
+    }
+
+    printf("PostgreSQL version: %s\n", PQgetvalue(res, 0, 0));
+    PQclear(res);
     PQfinish(conn);
 
     return 0;
